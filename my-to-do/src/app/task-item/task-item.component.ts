db00aea58f16831e6aae4a4b5c0b64f968c5cfe2
Jala Aguirre <jbaguirre1@up.edu.ph>
2025-05-13 22:32:05 +0800

    Modification in task items
    Added UI checkbox and buttons in the task items, generated task services

--- my-to-do/src/app/task-item/task-item.component.ts
+++ my-to-do/src/app/task-item/task-item.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../Task'; 
 import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,
+            MatCheckboxModule,
+            MatIconModule,
+            MatButtonModule
+  ],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
