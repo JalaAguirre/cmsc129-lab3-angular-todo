16f3971fcbcc4e39edd550dbb5156484e55429c1
Jala Aguirre <jbaguirre1@up.edu.ph>
2025-05-16 23:02:04 +0800

    Added a delete component
    Create a delete toast component

---
+++ my-to-do/src/app/delete-task-toast/delete-task-toast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-task-toast',
+  imports: [],
+  templateUrl: './delete-task-toast.component.html',
+  styleUrl: './delete-task-toast.component.css'
+})
+export class DeleteTaskToastComponent {
+
+}
